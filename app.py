50bdb83270a66d71798444756014728c4eda019e
Ayush <abairagi311@gmail.com>
2018-06-12 12:08:05 +0530

    added postgresql

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'pin.sqlite')
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
