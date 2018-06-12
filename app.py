345ee7509a548880d6dc0d0257f749dd67c095f4
Ayush <abairagi311@gmail.com>
2018-06-12 12:15:20 +0530

    added migrations

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate, MigrateCommand
 import os
+from flask_script import Manager
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +17,9 @@ POSTGRES = {
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 ma = Marshmallow(app)
 
 
@@ -90,4 +95,5 @@ def post_location():
 
 
 if __name__ == '__main__':
+    manager.run()
     app.run(debug=True)
